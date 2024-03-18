2f684e52253a608d446dec72b5eaad1a7d52555b
joseluisvarce <163854785+joseluisvarce@users.noreply.github.com>
2024-03-18 14:35:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/ Script para cambiar el color del fondo de forma aleatoria
+
+const colores = [
+  "#007bff",
+  "#009688",
+  "#4caf50",
+  "#8bc34a",
+];
+
+const fondo = document.querySelector(".fondo");
+
+function cambiarColor() {
+  const colorAle
