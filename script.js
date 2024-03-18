4769c0db432d9a03eeb27762d09cc78a86038bf0
joseluisvarce <163854785+joseluisvarce@users.noreply.github.com>
2024-03-18 19:24:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
-const numSpheres = 20; // Número de esferas
-const container = document.querySelector('.container');
+const numSpheres = 30; // Número de esferas
+const background = document.querySelector('.background');
 
 // Crear las esferas
 for (let i = 0; i < numSpheres; i++) {
   const sphere = document.createElement('div');
   sphere.classList.add('sphere');
-  container.appendChild(sphere);
+  background.appendChild(sphere);
 }
-
-// Animación de las esferas
-const spheres = document.querySelectorAll('.sphere');
-spheres.forEach(sphere => {
-  const size = Math.random() * 50 + 10; // Tamaño aleatorio
-  const duration = Math.random() * 5 + 3; // Duración de la animación aleatoria
-
-  sphere.style.width = `${size}px`;
-  sphere.style.height = `${size}px`;
-  sphere.style.opacity = `${Math.random() * 0.5 + 0.3}`; // Opacidad aleatoria
-
-  const randomX = Math.random() * window.innerWidth;
-  const randomY = Math.random() * window.innerHeight;
-
-  sphere.style.left = `${randomX}px`;
-  sphere.style.top = `${randomY}px`;
-
-  const randomDelay = Math.random() * 3; // Retraso aleatorio
-  sphere.style.animation = `move ${duration}s linear ${randomDelay}s infinite`;
-
-  container.appendChild(sphere);
-});
