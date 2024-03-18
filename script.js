0b358157e084bb03acb3e5ae4bec6a646087a317
joseluisvarce <163854785+joseluisvarce@users.noreply.github.com>
2024-03-18 17:24:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-document.getElementById("message-form").addEventListener("submit", function(event) {
+document.getElementById("contact-form").addEventListener("submit", function(event) {
   event.preventDefault();
-
-  // Simplemente muestra un mensaje de éxito
-  document.getElementById("message-success").style.display = "block";
-
-  // Aquí se puede agregar código para enviar el formulario por correo electrónico
-  // Sin embargo, para hacer esto, se requeriría una implementación del lado del servidor
+  
+  // Simulación de envío de correo electrónico (sin backend)
+  setTimeout(function() {
+    document.getElementById("message-success").style.display = "block";
+  }, 2000); // Simular un retraso de 2 segundos para el envío del mensaje
 });
