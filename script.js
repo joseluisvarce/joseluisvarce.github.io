61766ef27d4a2f199454a8de49870dcb41baabc8
joseluisvarce <163854785+joseluisvarce@users.noreply.github.com>
2024-03-20 02:11:58 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const numSpheres = 10; // Número de esferas
-const background = document.querySelector('.background');
-
-// Crear las esferas
-for (let i = 0; i < numSpheres; i++) {
-  const sphere = document.createElement('div');
-  sphere.classList.add('sphere');
-  background.appendChild(sphere);
-}
